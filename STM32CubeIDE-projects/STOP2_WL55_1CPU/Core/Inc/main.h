8eeddc8b7f999068febf4f0d4f2841dfa6d506c0
Matteo <matteoandria@gmail.com>
2025-06-13 16:44:09 +0200

    STM32 project, testing STOP2 mode with RTC interrupt. Sleep duration and LED blinking at wake up changing at UB1 pressing. RTC working at LSI RC-32kHz, CPU works at 48 MHz.

---
+++ STM32CubeIDE-projects/STOP2_WL55_1CPU/Core/Inc/main.h
@@ -0,0 +1,92 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32wlxx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define BLED_Pin GPIO_PIN_15
+#define BLED_GPIO_Port GPIOB
+#define RCC_OSC32_IN_Pin GPIO_PIN_14
+#define RCC_OSC32_IN_GPIO_Port GPIOC
+#define GLED_Pin GPIO_PIN_9
+#define GLED_GPIO_Port GPIOB
+#define RCC_OSC32_OUT_Pin GPIO_PIN_15
+#define RCC_OSC32_OUT_GPIO_Port GPIOC
+#define FE_CTRL3_Pin GPIO_PIN_3
+#define FE_CTRL3_GPIO_Port GPIOC
+#define UB1_Pin GPIO_PIN_0
+#define UB1_GPIO_Port GPIOA
+#define UB1_EXTI_IRQn EXTI0_IRQn
+#define FE_CTRL2_Pin GPIO_PIN_5
+#define FE_CTRL2_GPIO_Port GPIOC
+#define FE_CTRL1_Pin GPIO_PIN_4
+#define FE_CTRL1_GPIO_Port GPIOC
+#define RLED_Pin GPIO_PIN_11
+#define RLED_GPIO_Port GPIOB
+#define T_VCP_RX_Pin GPIO_PIN_3
+#define T_VCP_RX_GPIO_Port GPIOA
+#define T_VCP_RXA2_Pin GPIO_PIN_2
+#define T_VCP_RXA2_GPIO_Port GPIOA
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
