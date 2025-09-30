5419011f591c35cba23f5631fc35ea7d2fa52eb5
Matteo ANDRIATSILAVO <matteoandria@gmail.com>
2025-09-30 17:10:09 +0200

    Sleep mode portated to Arduino (untested).

---
+++ LowPowerModes_WL55JC1OnArduino/SleepMode/STOP1Mode/main/Sleep_MX_Config.cpp
@@ -0,0 +1,55 @@
+/**
+  ******************************************************************************
+  * @file           : Sleep_MX_Config.cpp
+  * @brief          : This file provides code for the peripherals configurations, 
+  *                   RTC and GPIO here.
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32wlxx_hal.h"
+#include "Sleep_MX_Config.h"
+#include "SleepMode.h"
+#include "My_Error_Handler.h"
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+/*static*/ void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  /* USER CODE BEGIN MX_GPIO_Init_1 */
+
+  /* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, BLED_Pin|RLED_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOC, FE_CTRL3_Pin|FE_CTRL2_Pin|FE_CTRL1_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : BLED_Pin RLED_Pin */
+  GPIO_InitStruct.Pin = BLED_Pin|RLED_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : FE_CTRL3_Pin FE_CTRL2_Pin FE_CTRL1_Pin */
+  GPIO_InitStruct.Pin = FE_CTRL3_Pin|FE_CTRL2_Pin|FE_CTRL1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  /* USER CODE BEGIN MX_GPIO_Init_2 */
+
+  /* USER CODE END MX_GPIO_Init_2 */
+}
---
+++ LowPowerModes_WL55JC1OnArduino/SleepMode/STOP1Mode/main/main.ino
@@ -0,0 +1,137 @@
+/**
+  ******************************************************************************
+  * @file           : main.ino
+  * @brief          : Main program body, enters Sleep mode for a specified time
+                      and wakes up using RTC alarm set (every 7 seconds by default).
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32wlxx_hal.h"
+#include "SleepMode.h"
+#include "Sleep_SystemClock_Config.h"
+#include "Sleep_MX_Config.h"
+#include <STM32RTC.h>
+
+/* RTC related variables -----------------------------------------------------*/
+STM32RTC& rtc = STM32RTC::getInstance();
+volatile bool rtcAlarmFlag = false;
+volatile uint16_t SLEEP_DURATION = 7;
+/**
+  * @brief RTC interrupt callback function, updates RTC alarm flag by rising it.
+  * @note This function is called after RTC fires its itnerrupt.
+  * @param data: data that may have been stored in RTC, unused by default.
+  * @retval None
+ */
+void RTCAlarmCallback(void *data) {
+  UNUSED(data);
+  rtcAlarmFlag = true;
+}
+
+/**
+  * @brief Sets up the RTC through STM32duino RTC library API and functions, 
+  * and the initial sleep duration DEALY_S.
+  * @note This function should be called in the setup loop.
+  * @param delay_s: sleep duration offset in seconds for the first set up.
+  * @retval None
+ */
+void RTC_setup(uint16_t delay_s){
+  rtc.begin();
+  rtc.attachInterrupt(RTCAlarmCallback);
+  // Set initial time and date (optional, for demo)
+  rtc.setTime(0, 0, 0);
+  rtc.setDate(1, 1, 25);
+
+  uint8_t hours = rtc.getHours();
+  uint8_t minutes = rtc.getMinutes();
+  uint8_t seconds = rtc.getSeconds();
+  uint32_t subSeconds = rtc.getSubSeconds();
+
+  uint32_t newSubSeconds = subSeconds + SUBSECONDS_OFFSET;
+  uint8_t newSeconds = seconds + delay_s;
+
+  if (newSubSeconds >= 1000) { // assuming 1000 ms per second
+    newSubSeconds -= 1000;
+    newSeconds += 1;
+  }
+  if (newSeconds >= 60) {
+    newSeconds -= 60;
+    minutes += 1;
+    if (minutes >= 60) {
+        minutes = 0;
+        hours = (hours + 1) % 24;
+    }
+  }
+  rtc.setAlarmTime(hours, minutes, newSeconds, newSubSeconds);
+  rtc.enableAlarm(rtc.MATCH_HHMMSS);
+}
+
+/**
+  * @brief Enter Sleep mode for DELAY_S seconds,
+  * wake up on RTC alarm interrupt.
+  * @param delay_s: sleep duration offset in seconds
+  * @retval None
+ */
+ void Enter_SleepMode_WithRTCAlarm(uint16_t delay_s){
+  // Get current time
+  uint8_t hours = rtc.getHours();
+  uint8_t minutes = rtc.getMinutes();
+  uint8_t seconds = rtc.getSeconds();
+  uint32_t subSeconds = rtc.getSubSeconds();
+
+  // Calculate new alarm time
+  // Subseconds offset to consider the time between setting the alarm and actually enter the low-power mode
+  uint32_t newSubSeconds = subSeconds + SUBSECONDS_OFFSET;
+  uint8_t newSeconds = seconds + delay_s;
+
+  if (newSubSeconds >= 1000) { // assuming 1000 ms per second
+      newSubSeconds -= 1000;
+      newSeconds += 1;
+  }
+  if (newSeconds >= 60) {
+      newSeconds -= 60;
+      minutes += 1;
+      if (minutes >= 60) {
+          minutes = 0;
+          hours = (hours + 1) % 24;
+      }
+  }
+
+  rtc.setAlarmTime(hours, minutes, newSeconds, newSubSeconds);
+  rtc.enableAlarm(rtc.MATCH_HHMMSS);
+
+  Sleep_Entry_LEDSequence();  // Indicate the entry in STOP2 mode with the LED sequence
+
+  rtcAlarmFlag = false;
+
+  HAL_SuspendTick();          // Suspend the SysTick Increment
+
+  HAL_PWREx_EnterSLEEPMode(PWR_MAINREGULATOR_ON, PWR_SLEEPENTRY_WFI);
+  /* ####----####----####----####
+	 * MCU is asleep in Sleep mode
+	 * ####----####----####----####
+	 */
+
+  HAL_ResumeTick();           // Resume the SysTick Increment
+  
+  // SystemClock_Config();    // Uncomment to reset System clock configuration -- IF NECESSARY
+
+  Sleep_Exit_LEDSequence();   // Indicate the exit in STOP2 mode with the LED sequence
+
+  // Wait for alarm flag (should be set by callback)
+  while (!rtcAlarmFlag) {
+      // Optionally, sleep or do nothing
+  }
+}
+
+void setup() {
+  HAL_Init();
+  SystemClock_Config();
+  MX_GPIO_Init();
+  RTC_setup(SLEEP_DURATION);
+
+}
+
+void loop(){
+  Enter_SleepMode_WithRTCAlarm(SLEEP_DURATION);
+}
